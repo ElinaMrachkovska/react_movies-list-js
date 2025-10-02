583ce959fd1673262037da783c439bd71c7f79f6
Elina <elina@MacBook-Pro-Elina.local>
2025-10-02 16:26:50 +0300

    develop

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,7 @@
 import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = movies => (
+export const MovieList = ({movies}) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
